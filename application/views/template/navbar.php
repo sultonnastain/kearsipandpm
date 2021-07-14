46456d4ad821064aee9d66e82b81f43a211bcef3
sultonnastain <ahmadsulton050699@gmail.com>
2021-07-14 13:23:46 +0700

    update logo

--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -82,8 +82,10 @@
                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $this->session->userdata('level');?></span>
+                    Biro Kesekretariatan
                     <img class="img-profile rounded-circle"
-                        src="<?=base_url()?>assets/img/sulton.jpg">
+                        src="<?=base_url()?>assets/img/biro.png">
+            
                 </a>
                 <!-- Dropdown - User Information -->
                 <!-- <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
